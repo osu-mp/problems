226ec619400f438370964791e5a55b9acaeca79c
Matthew Pacey <paceym@oregonstate.edu>
2024-01-09 17:41:23 -0800

    Easy binary depth

---
+++ easy/104_max_depth_binary.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    """
+    Runtime
+Details
+41ms
+Beats 80.32%of users with Python3
+Memory
+Details
+18.55MB
+Beats 57.79%of users with Python3
+    """
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
