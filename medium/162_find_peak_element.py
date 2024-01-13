da3e7152392b3c0c621461485fe5ae9ac98129fb
Matthew Pacey <paceym@oregonstate.edu>
2024-01-12 16:04:39 -0800

    Medium find peak element

---
+++ medium/162_find_peak_element.py
@@ -0,0 +1,28 @@
+"""
+Runtime
+49
+ms
+Beats
+68.50%
+of users with Python3
+Memory
+17.50
+MB
+Beats
+26.65%
+of users with Python3
+"""
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                right = mid - 1
+            elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                return mid
+            
