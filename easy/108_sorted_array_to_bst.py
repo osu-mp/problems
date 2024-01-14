05994eff46ed5f4694e16e1ab9608b1726d35e59
Matthew Pacey <paceym@oregonstate.edu>
2024-01-14 09:40:57 -0800

    Easy sorted list to BST

---
+++ easy/108_sorted_array_to_bst.py
@@ -0,0 +1,35 @@
+
+"""
+Runtime
+49
+ms
+Beats
+95.54%
+of users with Python3
+Memory
+18.50
+MB
+Beats
+41.20%
+of users with Python3
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: list[int]) -> Optional[TreeNode]:
+        def helper(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            root = TreeNode(nums[mid])
+            root.left = helper(left, mid - 1)
+            root.right = helper(mid + 1, right)
+            return root
+        
+        return helper(0, len(nums) - 1)
